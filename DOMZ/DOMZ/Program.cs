eca0d225201e895017c0826bfa2af9b403ad275b
4POST <4post_4@mail.ru>
2020-08-15 13:33:12 +0300

    CommitX

---
+++ DOMZ/DOMZ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача №24
+            double a;
+            double b;
+            Console.WriteLine("Ведите катет a:")
+            Console.WriteLine("Ведите катет b:")
+
+
+
+
+        }
+    }
+}
---
+++ DOMZ1/DOMZ1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача №8
+            // Положили срочный депозит на квартал (3 месяца) под 24% годовых. Закрыли депозит через 4 месяца
+            double size_deposit;
+            double interest_on_deposits = 24; //Процент по депозиту задан условиями
+            Console.WriteLine("Ведите размер депозита:");
+            size_deposit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Ваш депозит:{size_deposit}");
+            Console.WriteLine($"Процент по депозиту:{interest_on_deposits} % годовых");
+            //Сколько средств на счету будет через 4 месяца
+            double profit = size_deposit + (size_deposit * interest_on_deposits / 100) / 3;
+            Console.WriteLine($"Через 4 месяца Вы получите:{profit}");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
